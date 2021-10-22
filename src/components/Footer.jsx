a32e137ad6917c60d4a8b6ce86a27eb0d27e4f55
Carlstong <carlstongrefalde@gmail.com>
2021-10-22 21:20:14 +0800

    added search func

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Footer = () => {
+    const [date, setDate] = useState();
+    const getYear = () => setDate(new Date().getFullYear())
+
+    useEffect(() =>{
+        getYear();
+    }, [])
+
     return (
         <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
-           <h1>2021 Googool Inc.</h1>
+           <h1>&copy; {date} Googool Inc.</h1>
+           <p>Carlston G.</p>
         </div>
     )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import { useDebounce } from 'use-debounce'
+
+import {useStateContext } from '../context/StateContextProvider'
+import { Links } from './Links'
 
 const Search = () => {
+    const [text, setText] = useState('Elon Musk');
+    const {setSearchTerm} = useStateContext ();
+    const [debouncedValue] = useDebounce(text, 300)
+
+    useEffect(() => {
+        if(debouncedValue) setSearchTerm(debouncedValue)
+    }, [debouncedValue])
+
     return (
-        <div>
-            Search
+        <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
+            <input 
+            type="text" 
+            value={text}
+            className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
+            placeholder="Search Googool or type url"
+            onChange={(e) => setText(e.target.value)}
+            />
+            {!text && (<button type="button"
+            className="absolute top-1.5 right-4 text-2xl text-gray-500"onClick={() => setText('')}>
+                    X
+            </button>)}
+            <Links/>
         </div>
     )
 }
